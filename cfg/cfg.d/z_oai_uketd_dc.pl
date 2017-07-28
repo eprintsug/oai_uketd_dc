651b6480f377a302a9a82b82ceb003b2d72dc1e2
jesusbagpuss <j.salter@leeds.ac.uk>
2017-07-28 14:47:49 +0100

    Add current version from EPrints Core and basic config files

---
+++ cfg/cfg.d/z_oai_uketd_dc.pl
@@ -0,0 +1,6 @@
+# Config file (editable by EPMC screen)
+
+
+# Map this plugin over the one supplied with EPrints
+$c->{plugin_alias_map}->{"Export::OAI_UKETD_DC"} = "Export::OAI_UKETD_DC_2017";
+$c->{plugin_alias_map}->{"Export::OAI_UKETD_DC_2017"} = undef;
---
+++ lib/plugins/EPrints/Plugin/Screen/EPMC/OAI_UKETD_DC_2017.pm
@@ -0,0 +1,51 @@
+package EPrints::Plugin::Screen::EPMC::OAI_UKETD_DC_2017;
+
+@ISA = ( 'EPrints::Plugin::Screen::EPMC' );
+
+use strict;
+
+sub new
+{
+	my( $class, %params ) = @_;
+
+	my $self = $class->SUPER::new( %params );
+
+	$self->{package_name} = 'uketd_dc';
+	$self->{actions} = [qw( enable disable configure )];
+	$self->{disable} = 0; # always enabled, even in lib/plugins
+
+	return $self;
+}
+
+sub allow_configure { shift->can_be_viewed( @_ ) }
+
+sub action_configure
+{
+	my( $self ) = @_;
+
+	my $epm = $self->{processor}->{dataobj};
+	my $epmid = $epm->id;
+
+	foreach my $file ($epm->installed_files)
+	{
+		my $filename = $file->value( "filename" );
+		next if $filename !~ m#^epm/$epmid/cfg/cfg\.d/(.*)#;
+
+		my $url = $self->{repository}->current_url( host => 1 );
+		$url->query_form(
+			screen => "Admin::Config::View::Perl",
+			configfile => "cfg.d/$1",
+		);
+
+		$self->{repository}->redirect( $url );
+
+		exit( 0 );
+	}
+
+	$self->{processor}->{screenid} = "Admin::EPM";
+
+	$self->{processor}->add_message( "error", $self->html_phrase( "missing" ) );
+
+}
+
+1;
